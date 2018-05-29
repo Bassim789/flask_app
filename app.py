2d4edf2674ebc14d66c15638acbec32b7b123dbc
Bassim789 <bassim.matar@gmail.com>
2018-05-30 00:10:05 +0200

    ok

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'Perfect! top!'
+    return 'Perfect! ok!'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
