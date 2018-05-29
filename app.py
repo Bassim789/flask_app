17f0a1a00c2cf8618a80a37e0d4a12576f36de97
Bassim789 <bassim.matar@gmail.com>
2018-05-30 00:49:38 +0200

    ok

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
+server_ip = '137.74.172.99'
 
 @app.route("/")
 def index():
-    return 'Perfect! ok!'
+    return 'Perfect! ok2!'
 
 if __name__ == "__main__":
     app.run(debug=True)
